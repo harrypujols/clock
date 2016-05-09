428761e6b32e1f6ee8a9e57b619724a01c9e9a17
Harry Pujols <harry.pujols@pgmmb.com>
2016-05-09 12:23:29 -0400

    using api for date

--- js/app.js
+++ js/app.js
@@ -9,9 +9,6 @@ new Vue({
     current: {},
     forecast: {},
     clock: '00:00:00',
-    weekday: new Date().getDay(),
-    month: new Date().getMonth(),
-    date: new Date().getDate(),
     prefs: {},
     pm: false,
     expand: false
@@ -20,7 +17,6 @@ new Vue({
   ready: function() {
     this.settings()
     this.geolocation()
-    this.parsedate()
   },
 
   watch: {
@@ -102,71 +98,6 @@ new Vue({
     addzero: function(i) {
       if (i < 10) {i = "0" + i}
       return i
-    },
-
-    parsedate: function() {
-      switch (this.weekday) {
-          case 0:
-              this.weekday = "Sun"
-              break
-          case 1:
-              this.weekday = "Mon"
-              break
-          case 2:
-              this.weekday = "Tue"
-              break;
-          case 3:
-              this.weekday = "Wed"
-              break;
-          case 4:
-              this.weekday = "Thu"
-              break;
-          case 5:
-              this.weekday = "Fri"
-              break;
-          case 6:
-              this.weekday = "Sat"
-              break;
-      }
-
-      switch (this.month) {
-          case 0:
-              this.month = "Jan"
-              break
-          case 1:
-              this.month = "Feb"
-              break
-          case 2:
-              this.month = "Mar"
-              break
-          case 3:
-              this.month = "Apr"
-              break
-          case 4:
-              this.month = "May"
-              break
-          case 5:
-              this.month = "Jun"
-              break
-          case 6:
-              this.month = "Jul"
-              break
-          case 7:
-              this.month = "Aug"
-              break
-          case 8:
-              this.month = "Sep"
-              break
-          case 9:
-              this.month = "Oct"
-              break
-          case 10:
-              this.month = "Nov"
-              break
-          case 11:
-              this.month = "Dec"
-              break
-      }
     },
 
     settings: function() {
